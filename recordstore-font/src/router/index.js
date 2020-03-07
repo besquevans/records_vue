0a99db4436f9acb8c9344956a17ec6d54ee48dbc
Jimmy <du20783@gmail.com>
2020-03-07 22:04:34 +0800

    add signup vue

--- recordstore-font/src/router/index.js
+++ recordstore-font/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Signin from "@/components/Signin.vue";
+import Signup from "@/components/Signup.vue";
+
 Vue.use(Router);
 
 export default new Router({
@@ -10,6 +12,11 @@ export default new Router({
       path: "/",
       name: "Signin",
       component: Signin
+    },
+    {
+      path: "/signup",
+      name: "Signup",
+      component: Signup
     }
   ]
 });
