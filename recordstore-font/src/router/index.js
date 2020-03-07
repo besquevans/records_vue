6f140414b0071bea3d16b3bc37e8bfe6a6662e83
Jimmy <du20783@gmail.com>
2020-03-07 21:53:28 +0800

    add sign vue

--- recordstore-font/src/router/index.js
+++ recordstore-font/src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
-Vue.use(Router)
+import Vue from "vue";
+import Router from "vue-router";
+import Signin from "@/components/Signin.vue";
+Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "Signin",
+      component: Signin
     }
   ]
-})
+});
